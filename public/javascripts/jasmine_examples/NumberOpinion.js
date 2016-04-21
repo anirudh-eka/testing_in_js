9887f0315fbf5ac49685bee225e505fb79f2d391
adhulip <rudysback@gmail.com>
2016-04-21 00:10:14 -0400

    make semantically private function truly private

--- public/javascripts/jasmine_examples/NumberOpinion.js
+++ public/javascripts/jasmine_examples/NumberOpinion.js
@@ -4,7 +4,7 @@ NumberOpinion.prototype.isANumberILike = function(num) {
   if(num == 12){
     throw new Error("12 shouldnt be a number");
   }
-  return this.__isDivisibleBy3(num);
+  return __isDivisibleBy3(num);
 };
 NumberOpinion.prototype.getLuckyNumber = function() {
   return 18;
@@ -13,12 +13,12 @@ NumberOpinion.prototype.getLuckyNumber = function() {
 NumberOpinion.prototype.getRandomAcceptableNumber = function() {
   while(true) {
     var num = Math.floor(Math.random() * 100);
-    if (this.__isDivisibleBy3(num) && num % 2 == 0){
+    if (__isDivisibleBy3(num) && num % 2 == 0){
       return num
     }
   }
 };
 
-NumberOpinion.prototype.__isDivisibleBy3 = function(num) {
+var __isDivisibleBy3 = function(num) {
   return num % 3 == 0;
 };
